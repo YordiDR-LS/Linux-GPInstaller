72bfe483a292a09b91dfc8366207c0e49df75576
YordiDR-LS <yordi.de.rijcke@lansweeper.com>
2020-07-02 20:17:52 +0200

    install-GP

---
+++ install-GP.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function error-handle {
+if [ -z $3 ]; then
+  declare -ri expectedCode=0
+else
+  declare -ri expectedCode=1
+fi
+
+if [ $1 -ne $expectedCode ]; then
+  echo $2
+  echo 'Please try again, if this issue persists; submit a ticket to the IT Service Desk.'
+  exit 1
+fi
+}
+if [ $UID -ne 0 ]; then
+  echo 'This script requires root access to install packages.'
+  echo 'Please relaunch the script using sudo.'
+  exit 1
+fi
+
+echo 'Downloading GlobalProtect VPN Client installer...'
+sudo wget -O /tmp/GlobalProtectInstaller.deb https://github.com/YordiDR-LS/Linux-GPInstaller/blob/master/GlobalProtect_CLI-5.1.4.0-9.deb?raw=true &> /dev/null
+error-handle $? 'Something went wrong whilst downloading the installer.'
+
+echo 'Installing GlobalProtect VPN Client...'
+sudo dpkg -i /tmp/GlobalProtectInstaller.deb
+error-handle $? 'Something went wrong whilst installing the GlobalProtect VPN Client.'
+
+sudo rm /tmp/GlobalProtectInstaller.deb
+
+echo 'Downloading script to connect to VPN...'
+sudo wget -O /bin/lsvpn https://raw.githubusercontent.com/YordiDR-LS/Linux-GPInstaller/master/lsvpn.sh
+error-handle $? 'Something went wrong whilst downloading the script.'
+sudo chmod +x /bin/lsvpn
+error-handle $? 'Something went wrong whilst making the script executable.'
+
+echo 'Install complete!'
+echo "To connect to the Belgian Lansweeper VPN, enter 'lsvpn --connect'."
+echo "To disconnect from the Belgian Lansweeper VPN, enter 'lsvpn --disconnect'."
+
+exit 0
